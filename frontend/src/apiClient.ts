4abce18222de31f0c0132276b11d3c7883737e1b
ahg80 <ah.golrokhi@gmail.com>
2024-01-17 17:53:46 +0330

    editing base url

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   baseURL:
-    process.env.NODE_ENV === 'development' ? 'http://localhost:4000/' : '/',
+    process.env.NODE_ENV === 'development' ? 'https://online-shop-ts-mern-backend.vercel.app/' : '/',
   headers: {
     'Content-type': 'application/json',
   },
